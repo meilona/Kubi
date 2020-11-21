6bfc9df534af73f3859441521c62fce1d4d377f2
meilona <meilonaeurica@gmail.com>
2020-11-21 23:07:32 +0700

    jawab question

--- src/app/home/questions/questions.page.ts
+++ src/app/home/questions/questions.page.ts
@@ -2,6 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import {Question} from '../../models/question.model';
 import {ActivatedRoute, Router} from '@angular/router';
 import {QuestionService} from '../../services/question.service';
+import {map} from 'rxjs/operators';
+import {NavController} from '@ionic/angular';
+
+// interface Questions {
+//   // id: string;
+//   answer: string;
+//   questions: string;
+//   choice1: string;
+//   choice2: string;
+//   choice3: string;
+//   choice4: string;
+//   hint: string;
+// }
 
 @Component({
   selector: 'app-questions',
@@ -9,18 +22,106 @@ import {QuestionService} from '../../services/question.service';
   styleUrls: ['./questions.page.scss'],
 })
 export class QuestionsPage implements OnInit {
-  loadedQuestion: Question;
+  // CONSTANTS
+  rightScore = 10;
+  maxQuestions = 5;
+
+  // Question from firebase
+  loadedQuestion: any;
+  tempQuestion: any[];
+  questions: any[];
+  i: number;
+
+  currentQuestion: any;
+
+  rightAnswers = false;
+  score = 0;
+  questionCounter = 0;
+  availableQuesions: any[];
+  private question: HTMLElement;
+  private choices: Element[];
+
   constructor(
       private activatedRoute: ActivatedRoute,
-      private questionService: QuestionService
+      private questionService: QuestionService,
+      private navCtrl: NavController
   ) { }
 
   ngOnInit() {
-    this.activatedRoute.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('questionId')){ return; }
-      const questionId = paramMap.get('questionId');
-      this.loadedQuestion = this.questionService.getQuestion(questionId);
+    // get questions from firebase
+    this.questionService.getQuest().snapshotChanges().pipe(
+        map(changes =>
+            changes.map(c => ({data: c.payload.doc.data()}))
+        )
+    ).subscribe(data => {
+      this.questions = [ ];
+      this.tempQuestion = data;
+
+      for (this.i = 0; this.i < this.tempQuestion.length ; this.i++) {
+        this.loadedQuestion = this.tempQuestion[this.i].data;
+        const choices = this.loadedQuestion.listanswer.split(',');
+        const quest = {
+          kategori: this.loadedQuestion.kategori,
+          answer: this.loadedQuestion.answer,
+          question: this.loadedQuestion.question,
+          choice1: choices[0],
+          choice2: choices[1],
+          choice3: choices[2],
+          choice4: choices[3],
+          hint: this.loadedQuestion.hint,
+          materi: this.loadedQuestion.materi
+        };
+        this.questions.push(quest);
+      }
+      console.log(this.questions);
+      this.startGame();
     });
   }
 
+  ionViewWillEnter() {
+    this.question = document.getElementById('question');
+    this.choices = Array.from(document.getElementsByClassName('choice-text'));
+  }
+
+  startGame = () => {
+    this.questionCounter = 0;
+    this.score = 0;
+    this.availableQuesions = this.questions;
+    console.log(this.questions);
+    this.getNewQuestion();
+  }
+
+  getNewQuestion = () => {
+    if (this.availableQuesions.length === 0 || this.questionCounter >= this.maxQuestions) {
+      return this.navCtrl.navigateForward('/home');
+    }
+    this.questionCounter++;
+    const questionIndex = Math.floor(Math.random() * this.availableQuesions.length);
+    this.currentQuestion = this.availableQuesions[questionIndex];
+
+    this.question.innerText = 'Pertanyaan : ' + this.currentQuestion.question;
+
+    this.choices.forEach((choice) => {
+      const num = choice.dataset.number;
+      choice.innerHTML = this.currentQuestion['choice' + num];
+    });
+
+    this.availableQuesions.splice(questionIndex, 1);
+    // this.rightAnswers = true;
+  }
+
+  selectedAnswer(e){
+    const selectedChoice = e.target.innerText;
+    const selectedAnswer = this.currentQuestion.answer;
+    console.log(selectedAnswer + ' banding ' + selectedChoice);
+    if (selectedAnswer !== selectedChoice){
+      console.log('salah');
+      this.rightAnswers = false;
+    } else {
+      this.rightAnswers = true;
+      console.log('benar');
+    }
+    this.getNewQuestion();
+    this.rightAnswers = false;
+  }
 }
--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Question} from '../models/question.model';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {User} from '../models/user.model';
+
+// interface Questions {
+//   id: string;
+//   answer: string;
+//   questions: string;
+//   choice1: string;
+//   choice2: string;
+//   choice3: string;
+//   choice4: string;
+//   hint: string;
+// }
 
 @Injectable({
   providedIn: 'root'
@@ -36,8 +49,12 @@ export class QuestionService {
       choices3: 'pilihan3',
       choices4: 'pilihan4'
     }];
+  private dbPath = 'questions';
+  userRef: AngularFirestoreCollection<any>;
 
-  constructor() { }
+  constructor(
+      private db: AngularFirestore
+  ) { }
 
   getAllQuestion(){
     return[...this.question];
@@ -47,5 +64,12 @@ export class QuestionService {
     return {...this.question.find(question => {
       return question.id === questionId;
     })};
+  }
+
+  // ambil questions dari firebase
+  getQuest(): AngularFirestoreCollection<any>{
+    this.userRef = this.db.collection<any>(this.dbPath, ref => ref);
+    console.log(this.userRef);
+    return this.userRef;
   }
 }
