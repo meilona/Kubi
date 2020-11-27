bf3edf84111bf02f86998e1a52aba4ffdba84640
pdindaks <dinda.susilo@gmail.com>
2020-11-28 01:13:33 +0700

    dark mode

--- src/app/home/profile/setting/setting.page.ts
+++ src/app/home/profile/setting/setting.page.ts
@@ -12,4 +12,27 @@ export class SettingPage implements OnInit {
   ngOnInit() {
   }
 
+  onClick(event){
+    const systemDark = window.matchMedia('(prefers-color-scheme: dark)');
+    // @ts-ignore
+    systemDark.addListener(this.colorTest);
+    // systemDark.addListener(this.colorTest);
+    if (event.detail.checked){
+      document.body.setAttribute('data-theme', 'dark');
+      console.log('dark');
+    }
+    else{
+      document.body.setAttribute('data-theme', 'light');
+      console.log('light');
+    }
+  }
+
+  colorTest(systemInitiatedDark) {
+    if (systemInitiatedDark.matches) {
+      document.body.setAttribute('data-theme', 'dark');
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
 }
